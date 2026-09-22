45dbae455fa2cee6e4b53f27cf212b182a22adc8
0rndm_p <277301021+0rndm@users.noreply.github.com>
2026-09-22 18:03:59 -0300

    build things in parallel and compile files to .o then .wasm/js and make an entry point for goose and remove lua.js/wasm (rename them to goose.js/wasm)

--- build.sh
+++ build.sh
@@ -1,6 +1,48 @@
-emcc $(find lua -name "*.c" \
+#!/bin/bash
+set -e
+
+CFlags="-O0"
+Jobs="$(nproc)"
+
+rm -rf build
+mkdir build
+mkdir build/lua
+mkdir build/goose
+
+compile_lua() {
+  File="$1"
+  Object="build/lua/$(echo "$File" | sed 's#/#_#g; s#\.c$#.o#')"
+
+  echo "  CC  $File"
+  emcc $CFlags -c "$File" \
+    -Ilua \
+    -o "$Object"
+}
+
+compile_goose() {
+  File="$1"
+  Object="build/goose/$(echo "$File" | sed 's#/#_#g; s#\.c$#.o#')"
+
+  echo "  CC  $File"
+  emcc $CFlags -c "$File" \
+    -Ilua \
+    -o "$Object"
+}
+
+export -f compile_lua
+export -f compile_goose
+export CFlags
+
+find lua -name "*.c" \
   ! -name "onelua.c" \
   ! -name "lua.c" \
-  ! -name "lib*.c") \
+  ! -name "lib*.c" |
+  xargs -P "$Jobs" -n 1 bash -c 'compile_lua "$1"' _
+
+find goose -name "*.c" |
+  xargs -P "$Jobs" -n 1 bash -c 'compile_goose "$1"' _
+
+echo "  LD  web/goose.js"
+emcc $CFlags build/lua/*.o build/goose/*.o \
   -Ilua \
   -o web/goose.js
